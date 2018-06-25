2de8d86f13233b11eed450ae9e96afc7acc8710d
MrFoster81 <Jeremy-Foster@ouhsc.edu>
2018-06-25 16:55:06 -0500

    TEst save

---
+++ GitHub/widget-google-spreadsheet-master/src/widget/main.js
@@ -0,0 +1,16 @@
+require( "./css/spreadsheet.css" );
+
+const React = require( "react" ),
+  ReactDOM = require( "react-dom" ),
+  Main = require( "./components/main" );
+
+( function( window, document ) {
+  "use strict";
+
+  // Disable context menu (right click menu)
+  window.oncontextmenu = function() {
+    return false;
+  };
+
+  ReactDOM.render( <Main />, document.getElementById( "mainContainer" ) );
+} )( window, document );
